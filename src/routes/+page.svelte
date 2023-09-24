3c96b726bc1b28e9bf92978e945b41455942dda9
alanionita <test@gmail.com>
2023-09-24 13:05:31 +0100

    FEAT : implements the Segment component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,57 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import Segment from '../components/Segment.svelte';
+</script>
+
+<Segment type="indigo" class="indigo">
+	<h2>I'm Alan Ionita</h2>
+	<h3>Software Developer from Manchester.</h3>
+	<p>Working at <a href="https://www.perceptual-robotics.com">Perceptual Robotics</a><br/>Building industry 4.0 products.<p>
+</Segment>
+<Segment>
+	<h3> Experience with:</h3>
+	<ul>
+		<li>Web.</li>
+		<li>Backend</li>
+		<li>Embedded</li>
+		<li>Strategy</li>
+	</ul>
+</Segment>
+<Segment>
+	<h3>Teaching</h3>
+	<ul>
+		<li>
+			<p>Instructor at <a href="https://www.manchester.ac.uk/study/cpd/coding-bootcamp/">The University of Manchester</a>
+			<p>Teaching Fullstack Web Development</p>
+		</li>
+		<li>
+			<p>Mentor at <a href="https://codebar.io">Codebar</a></p>
+			<p>Free monthly programming workshops for diverse communities</p>
+		</li>
+		<li>
+			<p>Co-organiser of <a href="https://codeyourfuture.io">CodeYourFuture</a>
+			<p>Non-profit coding school for refugees</p>
+		</li>
+	</ul>
+</Segment>
+<Segment>
+	<p>Discover more on: <a href="https://www.linkedin.com/in/alanionita/">LinkedIn</a>
+</Segment>
+
+<style>
+.indigo {
+	composes: main;
+	height: auto;
+	padding-top: 35vh;
+}
+  
+.indigo > p {
+	margin: 0;
+	padding: 0;
+	width: auto;
+	color: var(--color-light-blue);
+	text-align: left;
+	font-weight: bold;
+	font-size: var(--fluid-type);
+}
+
+</style>
