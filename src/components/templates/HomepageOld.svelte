892298badcd79aa7317922956d8868d86345f9fe
Alan Ionita <105461667+alanionita@users.noreply.github.com>
2023-12-14 02:21:57 +0000

    STYLE (global) : adds new colour scheme and refactors everywhere
    Updates the link styles
    
    Refactors old classes that were defined by colours to be primary, secondary, tertiary
    
    Hoping this pattern will help me replace the themes with a lot more ease in the future

--- src/components/templates/HomepageOld.svelte
+++ src/components/templates/HomepageOld.svelte
@@ -2,39 +2,41 @@
 	import Segment from '../molecules/Segment/+index.svelte';
 </script>
 
-<Segment class="indigo">
-	<h1 class="indigo-text">I'm Alan Ionita,</h1>
-	<p class="indigo-text">Software Developer from 53.483959, -2.244644</p>
-	<p class="indigo-text">
+<Segment class="primary">
+	<h1 class="primary-text">I'm Alan Ionita,</h1>
+	<p class="primary-text">Software Developer from 53.483959, -2.244644.</p>
+	<p class="primary-text">
 		Working at <a class="link" href="https://www.perceptual-robotics.com">Perceptual Robotics</a>,
 		<br />building industry 4.0 products.
 	</p>
 </Segment>
-<Segment class="navy">
-	<h2 class="navy-text">Production experience:</h2>
-	<p class="navy-text">Web + Cloud + Backend + Strategy + Embedded.</p>
+<Segment class="secondary">
+	<h2 class="secondary-text">Production experience:</h2>
+	<p class="secondary-text">Web + Cloud + Backend + Strategy + Embedded.</p>
 </Segment>
-<Segment class="turquise">
-	<h3>Teaching experience:</h3>
-	<ul>
+<Segment class="tertiary">
+	<h2 class="tertiary-text">Teaching experience:</h2>
+	<ul class="tertiary-list">
 		<li>
-			<p>
-				Instructor at <a href="https://www.manchester.ac.uk/study/cpd/coding-bootcamp/"
+			<p class="tertiary-text">
+				Instructor at <a class="link--contrast" href="https://www.manchester.ac.uk/study/cpd/coding-bootcamp/"
 					>The University of Manchester</a
 				>
 			</p>
-			<p>Teaching Fullstack Web Development</p>
+			<p class="tertiary-text">Teaching Fullstack Web Development</p>
 		</li>
 		<li>
-			<p>Mentor at <a href="https://codebar.io">Codebar</a></p>
-			<p>Free monthly programming workshops for diverse communities</p>
+			<p class="tertiary-text">Mentor at <a class="link--contrast" href="https://codebar.io">Codebar</a></p>
+			<p class="tertiary-text">Free monthly programming workshops for diverse communities</p>
 		</li>
 		<li>
-			<p>Co-organiser of <a href="https://codeyourfuture.io">CodeYourFuture</a></p>
-			<p>Non-profit coding school for refugees</p>
+			<p class="tertiary-text">
+				Co-organiser of <a class="link--contrast" href="https://codeyourfuture.io">CodeYourFuture</a>
+			</p>
+			<p class="tertiary-text">Non-profit coding school for refugees</p>
 		</li>
 	</ul>
 </Segment>
-<Segment class="navy">
+<Segment class="secondary">
 	<p>Discover more on: <a href="https://www.linkedin.com/in/alanionita/">LinkedIn</a></p></Segment
 >
