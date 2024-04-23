0b2c7c80ad954fcf2449363894f0feab66256929
Alan Ionita <105461667+alanionita@users.noreply.github.com>
2024-04-23 15:06:16 +0100

    REFACTOR (Blog): abstracted the list into a separate component

---
+++ src/components/molecules/BlogList/BlogList.svelte
@@ -0,0 +1,43 @@
+<script>
+	import Link from '../../atoms/Link.svelte';
+</script>
+
+<ul class="list">
+    <li class="item">
+        <Link to="/1" text="Life OS" />
+        <aside>
+            <time datetime="2021-03-28T19:00">Posted on - Sunday, 28th, March, 2021</time>
+        </aside>
+    </li>
+    <li class="item">
+        <Link to="/2" text="Raspberry Pi OS Study" />
+        <aside>
+            <time datetime="2021-03-21T19:00">Posted on - Sunday, 21st March, 2021</time>
+        </aside>
+    </li>
+</ul>
+
+<style module="true">
+	.list {
+		list-style: 'none';
+		padding: 0;
+	}
+	.item {
+		display: flex;
+		flex-direction: column;
+		align-items: flex-start;
+		color: white;
+		font-size: var(--fluid-type-h2);
+		padding-bottom: 2rem;
+	}
+	.item:last-child {
+		padding-bottom: 0rem;
+	}
+	.item > aside {
+		color: var(--color-primary);
+		font-weight: 700;
+	}
+	.item-link {
+		text-decoration: underline;
+	}
+</style>
--- src/components/templates/Blog.svelte
+++ src/components/templates/Blog.svelte
@@ -1,6 +1,6 @@
 <script>
 	import Segment from '../molecules/Segment/+index.svelte';
-	import Link from '../atoms/Link.svelte';
+	import BlogList from '../molecules/BlogList/BlogList.svelte';
 </script>
 
 <Segment class="secondary">
@@ -8,43 +8,5 @@
 	<p class="secondary-text">Back to <a class="link" href="/">./home</a></p>
 </Segment>
 <Segment class="tertiary less-padding">
-	<ul class="list">
-		<li class="list-item">
-			<Link to="/1" text="Life OS" />
-			<aside>
-				<time datetime="2021-03-28T19:00">Posted on - Sunday, 28th, March, 2021</time>
-			</aside>
-		</li>
-		<li class="list-item">
-			<Link to="/2" text="Raspberry Pi OS Study" />
-			<aside>
-				<time datetime="2021-03-21T19:00">Posted on - Sunday, 21st March, 2021</time>
-			</aside>
-		</li>
-	</ul>
+	<BlogList/>
 </Segment>
-
-<style module="true">
-	.list {
-		list-style: 'none';
-		padding: 0;
-	}
-	.list-item {
-		display: flex;
-		flex-direction: column;
-		align-items: flex-start;
-		color: white;
-		font-size: var(--fluid-type-h2);
-		padding-bottom: 2rem;
-	}
-	.list-item:last-child {
-		padding-bottom: 0rem;
-	}
-	.list-item > aside {
-		color: var(--color-primary);
-		font-weight: 700;
-	}
-	.list-item-link {
-		text-decoration: underline;
-	}
-</style>
