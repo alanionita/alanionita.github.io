8d3cb492bdd226e0ce1dff89b3fdbb67ffc9ba3d
Alan Ionita <105461667+alanionita@users.noreply.github.com>
2024-04-23 15:28:14 +0100

    FEAT (BlogListItem): adds new comp BlogListItem
    Which takes props for: to, text, datetime, datestr
    
    Refactors BlogList to dynamically process blogPost data and render BlogListItem

--- src/components/molecules/BlogList/BlogList.svelte
+++ src/components/molecules/BlogList/BlogList.svelte
@@ -1,43 +1,32 @@
 <script>
-	import Link from '../../atoms/Link.svelte';
+	import BlogListItem from './BlogListItem.svelte';
+
+	let posts = [
+		{
+			to: '/1',
+			text: 'Life OS',
+			datetime: '2021-03-28T19:00',
+			datestr: 'Sunday, 28th, March, 2021'
+		},
+		{
+			to: '/2',
+			text: 'Raspberry Pi OS Study',
+			datetime: '2021-03-21T19:00',
+			datestr: 'Sunday, 21st March, 2021'
+		},
+		{
+			to: '/3',
+			text: 'Test',
+			datetime: '2021-03-21T19:00',
+			datestr: 'Sunday, 21st March, 2024'
+		}
+	];
 </script>
 
-<ul class="list">
-    <li class="item">
-        <Link to="/1" text="Life OS" />
-        <aside>
-            <time datetime="2021-03-28T19:00">Posted on - Sunday, 28th, March, 2021</time>
-        </aside>
-    </li>
-    <li class="item">
-        <Link to="/2" text="Raspberry Pi OS Study" />
-        <aside>
-            <time datetime="2021-03-21T19:00">Posted on - Sunday, 21st March, 2021</time>
-        </aside>
-    </li>
+<ul>
+	{#each posts as post}
+		<BlogListItem to={post.to} text={post.text} datetime={post.datetime} datestr={post.datestr}/>
+	{:else}
+		<li>No blog posts found</li>
+	{/each}
 </ul>
-
-<style module="true">
-	.list {
-		list-style: 'none';
-		padding: 0;
-	}
-	.item {
-		display: flex;
-		flex-direction: column;
-		align-items: flex-start;
-		color: white;
-		font-size: var(--fluid-type-h2);
-		padding-bottom: 2rem;
-	}
-	.item:last-child {
-		padding-bottom: 0rem;
-	}
-	.item > aside {
-		color: var(--color-primary);
-		font-weight: 700;
-	}
-	.item-link {
-		text-decoration: underline;
-	}
-</style>
---
+++ src/components/molecules/BlogList/BlogListItem.svelte
@@ -0,0 +1,32 @@
+<script>
+	import Link from '../../atoms/Link.svelte';
+    export let to = '';
+	export let text = '';
+	export let datetime = '';
+	export let datestr = '';
+</script>
+
+<li>
+    <Link to={to} text={text} />
+    <aside>
+        <time datetime={datetime}>Posted on - {datestr}</time>
+    </aside>
+</li>
+
+<style module="true">
+	li {
+		display: flex;
+		flex-direction: column;
+		align-items: flex-start;
+		color: white;
+		font-size: var(--fluid-type-h2);
+		padding-bottom: 2rem;
+	}
+	li:last-child {
+		padding-bottom: 0rem;
+	}   
+	time {
+		color: var(--color-primary);
+		font-weight: 700;
+	}
+</style>
