21e5fa86c2b4b855f57cb0d967ae8feb401daec5
Alan Ionita <105461667+alanionita@users.noreply.github.com>
2024-04-23 17:26:59 +0100

    TYPES (Post): adds new Post type

--- src/app.d.ts
+++ src/app.d.ts
@@ -12,7 +12,17 @@ declare global {
 			datetime: string
 			datestr: string
 		}
+		interface Post {
+			[index: string]: any | null;
+			id: string;
+			title: string;
+			desc: string;
+			updated: number;
+			created: number;
+			content: string;
+			contentHtml?: string;
+		};
 	}
 }
 
-export {};
+export { };
