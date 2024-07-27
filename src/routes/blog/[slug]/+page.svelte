ea8021fd6b0d86adb2e414ab7000918ae97e0804
Alan Ionita <105461667+alanionita@users.noreply.github.com>
2024-07-27 11:50:07 +0100

    REFACTOR (blog) : minor refactor to remvoe unused props

--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -1,9 +1,8 @@
 <script lang="ts">
 	import BlogPost from '../../../components/templates/BlogPost/+page.svelte';
 	import { page } from '$app/stores';
-	const slug: string = $page.params.slug || 'Not found';
 
 	export let data;
 </script>
 
-<BlogPost slug={slug} postHtml={data.html}/>
+<BlogPost postHtml={data.html} />
