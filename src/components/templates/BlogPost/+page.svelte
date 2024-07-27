6dd725ebf57d9ab799c4de196d45d1e6034ae672
Alan Ionita <105461667+alanionita@users.noreply.github.com>
2024-07-27 12:05:16 +0100

    FEAT (components/BlogPost) : adds highlight.js code syntax highlighting
    Refactors global.css to add comments and organise structure
    
    Add code-highlight.md file for testing the code highlights

--- src/components/templates/BlogPost/+page.svelte
+++ src/components/templates/BlogPost/+page.svelte
@@ -1,6 +1,18 @@
 <script lang="ts">
 	import Segment from '../../molecules/Segment/+index.svelte';
 	export let postHtml: string = '';
+	import hljs from 'highlight.js/lib/core';
+	import javascript from 'highlight.js/lib/languages/javascript';
+	import yaml from 'highlight.js/lib/languages/yaml';
+	import shell from 'highlight.js/lib/languages/shell';
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		hljs.registerLanguage('javascript', javascript);
+		hljs.registerLanguage('yaml', yaml);
+		hljs.registerLanguage('shell', shell);
+		hljs.highlightAll();
+	});
 </script>
 
 <Segment class="secondary no-padding">
@@ -13,7 +25,7 @@
 	.content {
 		margin: 6rem 0 0 0;
 		padding: 0;
-		width: auto;
+		width: 100%;
 		color: var(--color-text);
 		font-size: var(--fluid-type-post);
 	}
