1aa08a95c71706b9e544b659de99225f6ee95ff7
Alan Ionita <105461667+alanionita@users.noreply.github.com>
2024-07-27 13:19:13 +0100

    FIX (svelte.config.js): removed base

--- src/components/templates/Blog.svelte
+++ src/components/templates/Blog.svelte
@@ -5,7 +5,6 @@
 
 <Segment class="secondary">
 	<h1 class="secondary-text">Crystallising thoughts,<br />through writing.</h1>
-	<!-- <p class="secondary-text">Back to <a class="link" href="/">./home</a></p> -->
 </Segment>
 <Segment class="tertiary less-padding">
 	<BlogList/>
--- svelte.config.js
+++ svelte.config.js
@@ -5,23 +5,20 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const config = {
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		// adapter: adapter()
 		adapter: adapter({
 			// default options are shown. On some platforms
 			// these options are set automatically — see below
 			pages: 'build',
 			assets: 'build',
-			// fallback: '404.html',
+			fallback: 'index.html',
 			precompress: true, // TODO: toggle on/off and compare
 			strict: true
 		}),
 		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH,
-			relative: true
-			// base: 'https://alanionita.github.io/portfolio--svelte'
+			// base: process.argv.includes('dev') ? '' : process.env.BASE_PATH,
+		},
+		prerender: {
+			entries: []
 		}
 	},
 	preprocess: [vitePreprocess()]
