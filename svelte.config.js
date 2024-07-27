ea14f55b056a31e7046d9b4a553c3667d33c6e09
Alan Ionita <105461667+alanionita@users.noreply.github.com>
2024-07-27 12:50:33 +0100

    FIX (svelte.config.js): removed fallback

--- svelte.config.js
+++ svelte.config.js
@@ -14,7 +14,7 @@ const config = {
 			// these options are set automatically — see below
 			pages: 'build',
 			assets: 'build',
-			fallback: '404.html',
+			// fallback: '404.html',
 			precompress: true, // TODO: toggle on/off and compare
 			strict: true
 		}),
