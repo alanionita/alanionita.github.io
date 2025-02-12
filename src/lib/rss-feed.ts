cc20a0455e2985b7b9b9371edbcbe44bf3467b30
Alan Ionita <105461667+alanionita@users.noreply.github.com>
2025-02-12 01:30:55 +0000

    FEAT (rss): adds rss lib and implementation within entries
    Adds library to construct the rss feed
    
    Implements the lib in the blog/[slug] page entries so that xml gets created alongside slug map
    
    xml will be produced in /static/rss-feed.xml

---
+++ src/lib/rss-feed.ts
@@ -0,0 +1,36 @@
+import {parse, format} from 'date-fns'
+
+const SITE_LINK = "https://alanionita.github.io/portfolio--svelte/";
+
+export function makeXMLOutline(items: string): string {
+    const xmlEncoding = "UTF-8";
+    const xmlLang = "en-GB";
+    const title = "Alan Ionita - Fullstack developer porfolio, React, AWS, Typescript, bun";
+    const description = "Latest blog posts";
+
+    return `<?xml version="1.0" encoding="${xmlEncoding}"?>
+        <rss version="2.0">
+            <channel>
+                <title>${title}</title>
+                <link>${SITE_LINK}</link>
+                <description>${description}</description>
+                <language>${xmlLang}</language>
+                <lastBuildDate>${new Date().toUTCString()}</lastBuildDate>
+                ${items!}
+            </channel>
+        </rss>`;
+}
+
+export function makeXMLItems(data: App.Post[]): string {
+    return data.map((post: App.Post) => {
+        const dateObj = parse(post.created, 'dd/MM/yyyy', new Date());
+        const rfc822DateStr = format(dateObj, 'EEE, dd MMM yyyy HH:mm:ss XX');
+        return `<item>
+            <title>${post.title}</title>
+            <link>${SITE_LINK}/blog/${post.url}</link>
+            ${post.desc ? `<description>${post.desc}</description>` : ''}
+            <pubDate>${rfc822DateStr}</pubDate>
+            <author>Alan Ionita</author>
+        </item>`
+    }).join("")
+}
--- src/routes/blog/[slug]/+page.server.ts
+++ src/routes/blog/[slug]/+page.server.ts
@@ -1,10 +1,19 @@
 import { getPost, getPosts } from '$lib/posts';
 import { error } from '@sveltejs/kit';
 import type { RouteParams } from './$types';
+import { makeXMLItems, makeXMLOutline } from '$lib/rss-feed';
+import fs from 'fs';
+
+
+function makeXMLFeed(data: App.Post[]) {
+    const itemsStr = makeXMLItems(data)!;
+    const feedStr = makeXMLOutline(itemsStr);
+
+    fs.writeFileSync(`${process.cwd()}/static/rss-feed.xml`, feedStr);
+}
 
 export function entries() {
     const posts = getPosts();
-
     const postSlugs: RouteParams[] = posts.reduce((acc, post) => {
         if (post.slug && post.slug.length > 0) {
             acc.push({ slug: post.slug })
@@ -12,7 +21,8 @@ export function entries() {
         }
         return acc;
     }, [] as RouteParams[])
-    
+
+    makeXMLFeed(posts);
     return postSlugs;
 }
 
