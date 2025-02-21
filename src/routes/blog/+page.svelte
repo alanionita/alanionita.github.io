f33accc93163b43e4b0d9133293b74dbc10ebf64
Alan Ionita <105461667+alanionita@users.noreply.github.com>
2025-02-21 13:53:49 +0000

    REFACTOR: remove the context from Blog route, adds TS typings

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -1,29 +1,14 @@
-<script>
-	import { run } from 'svelte/legacy';
-
-	import { setContext } from 'svelte';
-	import { writable } from 'svelte/store';
+<script lang="ts">
 	import Blog from '../../components/templates/Blog.svelte';
 	import PageTitle from '../../components/atoms/PageTitle.svelte';
 
-	
-	/**
-	 * @typedef {Object} Props
-	 * @property {import('./$types').PageData} data
-	 */
-
-	/** @type {Props} */
-	let { data } = $props();
-
-	// Creates a posts state
-	const posts = writable();
-	run(() => {
-		posts.set(data.posts);
-	});
-
-	// Defines a posts context
-	setContext('posts', posts);
+	interface Props {
+		data: {
+			posts: App.Post[];
+		};
+	}
+	let { data }: Props = $props();
 </script>
 
-<PageTitle text="List of blog posts"/>
-<Blog/>
+<PageTitle text="List of blog posts" />
+<Blog posts={data.posts} />
